bd217289f463a8037db7e290dbfb6c6a5b21c739
Arash Abtahi <arash281pro@live.com>
2018-07-16 22:18:36 -0500

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -2,3 +2,9 @@ $('#arrow').click(function(){
         $('html, body').animate({scrollTop : 0},1000);
         return false;
     });
+
+$(window).resize(function(){
+ if($(window).width()<991){
+  $('.text-left').removeClass('text-left');
+ }
+});
